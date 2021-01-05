898bfcee2da35bb5e1f0560e3d5d4c0da5b2c84d
Jérôme WAX <j.wax@nadisolutions.com>
2021-01-05 20:08:14 +0100

    change multicase

--- openfaas-python3-ia/dl_dep.py
+++ openfaas-python3-ia/dl_dep.py
@@ -1,8 +1,8 @@
 from transformers import pipeline
 
-nlp = pipeline('question-answering', model='illuin/camembert-base-fquad', tokenizer='illuin/camembert-base-fquad')
+nlp = pipeline('question-answering', model='mrm8488/bert-multi-cased-finedtuned-xquad-tydiqa-goldp', tokenizer='mrm8488/bert-multi-cased-finedtuned-xquad-tydiqa-goldp')
 
-return nlp({
+nlp({
         'question': 'qui est le plus fort ?',
         'context': 'Jerôme wax est très fort'
-    })
+})
